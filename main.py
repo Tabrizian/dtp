1e93cc4c006b25b8516143a025ab0eb80c50a6a3
Iman Tabrizian <tabrizian@outlook.com>
2019-11-25 22:40:44 -0500

    Improve the main.py

--- main.py
+++ main.py
@@ -1,85 +1,59 @@
-'''Train CIFAR10 with PyTorch.'''
 import torch
 import torch.optim as optim
-import torchvision.transforms as transforms
 import torchvision
 import torch.distributed as dist
-import torch.nn.functional as F
 from torch.multiprocessing import Process
+from torch.functional import F
 import os
+import sys
 
 from random import Random
 from math import ceil
 
 
-from dtp.datasets.emnist import EMNIST
+from dtp.datasets.cifar_10 import CIFAR10
 import dtp.models.resnet as ResNet
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+EPOCHS = 10
+MASTER_IP = '127.0.0.1'
+MASTER_PORT = '8080'
+BATCH_SIZE = 30
 
-class Partition(object):
-
-    def __init__(self, data, index):
-        self.data = data
-        self.index = index
-
-    def __len__(self):
-        return len(self.index)
-
-    def __getitem__(self, index):
-        data_idx = self.index[index]
-        return self.data[data_idx]
-
-
-class DataPartitioner(object):
-
-    def __init__(self, data, sizes=[0.7, 0.2, 0.1], seed=1234):
-        self.data = data
-        self.partitions = []
-        rng = Random()
-        rng.seed(seed)
-        data_len = len(data)
-        indexes = [x for x in range(0, data_len)]
-        rng.shuffle(indexes)
-
-        for frac in sizes:
-            part_len = int(frac * data_len)
-            self.partitions.append(indexes[0:part_len])
-            indexes = indexes[part_len:]
-
-    def use(self, partition):
-        return Partition(self.data, self.partitions[partition])
 
 def partition_dataset():
-    dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True,
-                             transform=transforms.Compose([
-                                 transforms.RandomCrop(32, padding=4),
-                                 transforms.RandomHorizontalFlip(),
-                                 transforms.ToTensor(),
-                                 transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-                             ]))
     size = dist.get_world_size()
-    bsz = int(128 / float(size))
     partition_sizes = [1.0 / size for _ in range(size)]
-    partition = DataPartitioner(dataset, partition_sizes)
+    partition = CIFAR10(partition_sizes)
     partition = partition.use(dist.get_rank())
     train_set = torch.utils.data.DataLoader(partition,
-                                         batch_size=bsz,
+                                         batch_size=BATCH_SIZE,
                                          shuffle=True)
-    return train_set, bsz
+    return train_set, BATCH_SIZE
+
 
 def run(rank, size):
     torch.manual_seed(1234)
     train_set, bsz = partition_dataset()
+    print(len(train_set))
     model = ResNet.ResNet50()
-    optimizer = optim.SGD(model.parameters(),
-                          lr=0.01, momentum=0.5)
+    optimizer = optim.SGD(
+        model.parameters(),
+        lr=0.01, momentum=0.5
+    )
+    print('Rank', dist.get_rank())
 
     num_batches = ceil(len(train_set.dataset) / float(bsz))
+    train_set_size = len(train_set)
+
     for epoch in range(10):
         epoch_loss = 0.0
+        index = 0
         for data, target in train_set:
+            if index == 20:
+                break
+            index += 1
             optimizer.zero_grad()
             output = model(data)
             loss = F.nll_loss(output, target)
@@ -87,8 +61,10 @@ def run(rank, size):
             loss.backward()
             average_gradients(model)
             optimizer.step()
-        print('Rank ', dist.get_rank(), ', epoch ',
-              epoch, ': ', epoch_loss / num_batches)
+            print('Processed ', index, ' / ', train_set_size)
+            print('Rank ', dist.get_rank(), ', epoch ',
+                  epoch, ': ', epoch_loss / num_batches)
+
 
 def average_gradients(model):
     size = float(dist.get_world_size())
@@ -96,19 +72,34 @@ def average_gradients(model):
         dist.all_reduce(param.grad.data, op=dist.ReduceOp.SUM)
         param.grad.data /= size
 
+
 def init_process(rank, size, fn, backend='gloo'):
     """ Initialize the distributed environment. """
-    os.environ['MASTER_ADDR'] = '127.0.0.1'
-    os.environ['MASTER_PORT'] = '29500'
-    dist.init_process_group(backend, rank=rank, world_size=size)
+    os.environ['MASTER_ADDR'] = MASTER_IP
+    os.environ['MASTER_PORT'] = MASTER_PORT
+    os.environ['WORLD_SIZE'] = str(total_size)
+    dist.init_process_group(backend, rank=rank, world_size=total_size)
     fn(rank, size)
 
-size = 2
-processes = []
-for rank in range(size):
-    p = Process(target=init_process, args=(rank, size, run))
-    p.start()
-    processes.append(p)
 
-for p in processes:
-    p.join()
+if __name__ == '__main__':
+    # Number of cores available on each machine
+    size = int(sys.argv[3])
+
+    # Total number of workers to wait for
+    total_size = int(sys.argv[2])
+
+    # The current machine worker id
+    worker_id = int(sys.argv[1])
+
+    processes = []
+    print('World size is', total_size, 'and the worker id is', worker_id)
+    for rank in range(size):
+        p = Process(target=init_process, args=(rank + size * worker_id, total_size, run))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
