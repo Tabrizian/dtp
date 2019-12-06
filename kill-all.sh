4cf1be84470ae73b3613e02ea87a232f167d73da
Iman Tabrizian <tabrizian@outlook.com>
2019-12-05 23:06:08 -0500

    Add the missing files and final update

--- kill-all.sh
+++ kill-all.sh
@@ -1,3 +1,13 @@
+regex="[0-9]*:[0-9]* python3 /mnt/sharedfolder/dtp/main.py $1 $2"
+echo $regex
+processes=`ps aux | grep -e "$regex" | awk '{print $2}'`
+
+for process in $processes
+do
+    echo Killing process $process
+    sudo kill -9 $process
+done
+
 regex="[0-9]*:[0-9]* python3 main.py $1 $2"
 echo $regex
 processes=`ps aux | grep -e "$regex" | awk '{print $2}'`
